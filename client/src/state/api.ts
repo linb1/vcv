d16aafefb67f6f09041cd2f18c08469b3b0a3628
Brian Lin <brian@differential.com>
2024-08-16 18:19:24 -0400

    add initial frontend api for node

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,22 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export interface Nodes {
+  id: string;
+  prev: boolean;
+  name: string;
+  path: string;
+}
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+  tagTypes: ["Nodes"],
+  endpoints: (build) => ({
+    getNodes: build.query<Nodes, void>({
+      query: () => "/nodes",
+      providesTags: ["Nodes"],
+    }),
+  }),
 });
 
-export const {} = api;
+export const { useGetNodesQuery } = api;
