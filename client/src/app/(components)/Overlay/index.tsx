faed95aca76b0329b19aedf6de259c75720be5c7
Brian Lin <brian@differential.com>
2024-08-18 22:57:09 -0400

    add initial overlay with project info

---
+++ client/src/app/(components)/Overlay/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Overlay = () => {
+  return (
+    <div className="fixed h-screen w-1/5 min-w-60 bg-white shadow-md border border-slate-300">
+      <div className="flex flex-col justify-center items-center gap-20 px-5 py-20">
+        <h1 className="text-center text-3xl">Version Control Visualizer</h1>
+
+        <div>
+          This application can help visualize a version control by adding nodes
+          to the history. Interact with the nodes and comments to see how it
+          works!
+        </div>
+
+        <div>
+          <div>Instructions:</div>
+          <ul className="list-disc px-5">
+            <li>Drag to move around</li>
+            <li>Hover over a node to see the path to the original node</li>
+            <li>Click a node to open up the modal</li>
+            <li>Add a new node to the current node using the modal</li>
+            <li>
+              Add, edit, and view comments for the current node using the modal
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Overlay;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
 import NodeTree from "@/app/(components)/NodeTree";
+import Overlay from "@/app/(components)/Overlay";
 export default function Home() {
   return (
-    <div className="h-screen w-screen bg-slate-100">
+    <div className="h-screen w-screen bg-slate-100 relative">
+      <Overlay />
       <NodeTree />
     </div>
   );
