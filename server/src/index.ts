e2202139ae4017e5aa3af304b506f45b39646311
Brian Lin <brian@differential.com>
2024-08-19 19:49:07 -0400

    update setting in vercel and revert index

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
