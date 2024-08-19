a50a59a0987b0ce8ab5167dc9b87fbe3e4eece5c
Brian Lin <brian@differential.com>
2024-08-19 19:02:37 -0400

    add cross origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //Routes
 app.use("/nodes", nodeRoutes);
