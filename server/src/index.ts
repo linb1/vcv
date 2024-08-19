093ad96258de2c89f815842751bd8d0fd747a061
Brian Lin <brian@differential.com>
2024-08-19 19:56:25 -0400

    allow all origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,11 @@ import commentRoutes from "./routes/commentRoutes";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 // app.use(helmet());
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
