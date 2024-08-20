19c6c8b4e62c80562aee43f4d251ca16256926a0
Brian Lin <brian@differential.com>
2024-08-20 14:21:53 -0400

    add origin to cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,12 @@ import commentRoutes from "./routes/commentRoutes";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
