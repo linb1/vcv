63b344e6730d68be1eb0b8e8242074cbc63cb8f5
Brian Lin <brian@differential.com>
2024-08-19 21:59:26 -0400

    set up aws

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ app.use("/nodes", nodeRoutes);
 app.use("/comments", commentRoutes);
 
 //Server
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
