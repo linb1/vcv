bc4c21858794be9303dcbd2ca8f6bd938df96443
Brian Lin <brian@differential.com>
2024-08-20 13:06:08 -0400

    move cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,13 +10,13 @@ import commentRoutes from "./routes/commentRoutes";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
 //Routes
 app.use("/nodes", nodeRoutes);
