ff2c72a7a0a0d770140421f228f939b7f0272eb2
Brian Lin <brian@differential.com>
2024-08-20 15:05:50 -0400

    update overlay style

--- client/src/app/(components)/Overlay/index.tsx
+++ client/src/app/(components)/Overlay/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Overlay = () => {
   return (
     <div className="fixed h-screen w-1/5 min-w-60 bg-white shadow-md border border-slate-300">
-      <div className="flex flex-col justify-center items-center gap-10 px-5 py-14">
+      <div className="flex flex-col justify-center items-center gap-10 px-5 py-14 h-full overflow-scroll">
         <Network className="w-20 h-20" />
         <h1 className="text-center text-3xl">Version Control Visualizer</h1>
 
@@ -17,7 +17,7 @@ const Overlay = () => {
 
         <div>
           <div>Instructions:</div>
-          <ul className="list-disc px-5">
+          <ul className="list-disc px-5 text-sm">
             <li>Drag to move around</li>
             <li>Hover over a node to see the path to the original node</li>
             <li>Click a node to open up the modal</li>
@@ -25,7 +25,7 @@ const Overlay = () => {
             <li>
               Add, edit, and view comments for the current node using the modal
             </li>
-            <li>Delete a node and all it's descendant using the modal</li>
+            <li>Delete a node and all descendants using the modal</li>
           </ul>
         </div>
       </div>
